adb7a1d7dfc1aeb28c1a221c8057436786371787
lsb <leebutterman@gmail.com>
2020-11-06 13:11:45 -0800

    ensure git is installed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,15 +1,14 @@
 pip install transformers==2.8
 pip install -r ../hf_requirements.txt
 
+apt-get update
+apt-get install wget unzip git
+
 git clone https://github.com/NVIDIA/apex
 #Comment apex installation below if fp16 isn't required
 cd apex
 python setup.py install --cuda_ext --cpp_ext
 cd ..
-
-apt-get update
-apt-get install wget
-apt-get install unzip
 
 # transformers installation from source
 # git clone https://github.com/huggingface/transformers
